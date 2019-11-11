e455f1f8ab1f045409a32a3ef0aca2f9e7b59f67
Carter Figgins <carter.figgins@aggiemail.usu.edu>
2019-11-10 20:59:29 -0700

    Can add modles and serials

--- HookerApp/cards/urls.py
+++ HookerApp/cards/urls.py
@@ -5,9 +5,12 @@ from . import views
 app_name = 'cards'
 
 urlpatterns = [
-    path('', views.mainMenu, name='mainMenu'),
-    path('lookCards', views.lookCards, name='lookCards'),
-    path('addCards', views.addCards, name= 'addCards'),
-    path('lookAppliance', views.lookAppliance, name='lookAppliance'),
-    path('newCard',views.newCard, name='newCard'),
+	path('', views.mainMenu, name='mainMenu'),
+	path('lookCards', views.lookCards, name='lookCards'),
+	path('addCards', views.addCards, name= 'addCards'),
+	path('lookAppliance', views.lookAppliance, name='lookAppliance'),
+	path('newCard',views.newCard, name='newCard'),
+	path('newAppliance/<int:card_id>/', views.newAppliance, name='newAppliance'),
+	path('applianceView/<int:card_id>/', views.applianceView, name = 'applianceView'),
+	path('init', views.init, name = 'init'),
 ]
--- HookerApp/cards/views.py
+++ HookerApp/cards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
 from django.shortcuts import get_object_or_404, render
@@ -8,6 +8,18 @@ from django.urls import reverse
 
 def mainMenu(request):
 	return render(request, 'cards/MainCards.html')
+
+		
+def applianceView(request, card_id):
+	cards = get_object_or_404(Card, pk=card_id)
+	appliances = Appliance.objects.filter(card = cards)
+	context = {
+		"appliances" : appliances,
+		"cards" : cards,
+	}
+	
+	return render(request, 'cards/applianceView.html',context)
+
 
 
 def lookCards(request):
@@ -25,6 +37,8 @@ def addCards(request):
 	}
 	return render(request, 'cards/addCards.html', context )
 
+
+
 	
 def lookAppliance(request):
 	return render(request, 'cards/lookAppliance.html' )
@@ -36,24 +50,38 @@ def newCard(request):
 	applianceType = request.POST['type']
 	print(model)
 	if(model and brand and applianceType ):
-		saveCard = Card(modelNumber = model ,brand= request.POST['brand'] ,applianceType = request.POST['type'] , pub_date= timezone.now())
+		saveCard = Card(modelNumber = model ,brand= brand ,applianceType = applianceType, pub_date= timezone.now())
 		saveCard.save()
 		print("SUCCESS")
 	else:
 		print("FAILD")
-	# context = {
-	#     "saveCard" : saveCard,
-	#     "saved" : True,
-	# }
-	
-
 	return HttpResponseRedirect('/addCards')
+
+
+def newAppliance(request, card_id):
+	card = get_object_or_404(Card, pk=card_id)
+	serial = request.POST['serial']
+	unitCost = request.POST['unitCost']
+	classLevel = request.POST['classLevel']
+	if(serial and unitCost and classLevel):
+		saveAppliance = Appliance(card = card, serialNumber = serial, unitCost= unitCost, Class= classLevel, pub_date = timezone.now())
+		saveAppliance.save()
+	return redirect('/applianceView/' + str(card_id))
+		
+
 
 def init(request):
 	nuke(request)
-
+	for i in range(5):
+		card = Card(modelNumber = "MODEL" ,brand= "BRAND" ,applianceType = "TYPE" , pub_date= timezone.now())
+		card.save()
+		appliance = Appliance(card = card, serialNumber = "SERIAL", unitCost = 10.00, Class = "A CLASS", pub_date = timezone.now())
+		appliance.save()
+	return redirect('/')
 
 def nuke (request):
 	for q in Card.objects.all():
 		q.delete()
+
+
 	
